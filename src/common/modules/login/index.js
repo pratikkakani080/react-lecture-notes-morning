07f3ded69f4b1b86cdd3398fdc61ef9cb911c1a0
Your Name <you@example.com>
2024-09-03 10:29:45 +0530

    feat: login form validations

--- src/common/modules/login/index.js
+++ src/common/modules/login/index.js
@@ -1,53 +1,89 @@
 import React, { useState } from "react";
 import Button from "../../components/button";
 import { storeData } from "../../utils/storage";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   // const [email, setEmail] =useState('')
   // const [password, setPassword] =useState('')
   // const [userName, setUserName] =useState('')
 
-  const [userInfo, setUserInfo] = useState({})
-
-
-  // console.log('email****', email, 'password****', password, 'setUserName****', userName);
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState({});
+  const [errors, setErrors] = useState({});
 
   const handleChange = (event) => {
+    const { name, value, checked } = event.target;
     const userData = {
       ...userInfo,
-      [event.target.name]: event.target.value
-    }
-    setUserInfo(userData)
-    
+      [name]: name === "rememberMe" ? checked : value,
+    };
+    setUserInfo(userData);
+    setErrors({ ...errors, [name]: "" });
     // Object.assign(userInfo, userData)
     // setUserInfo((pre) => {return {...pre, userData}})
+  };
 
-  }
-  console.log('user*****', userInfo);
+  const validateForm = () => {
+    let isFormValid = true;
+    let errors = {};
+
+    if (!userInfo.email || userInfo.email.trim() === "") {
+      isFormValid = false;
+      errors.email = "* Email is required";
+    }
+
+    if (!userInfo.password || userInfo.password.trim() === "") {
+      isFormValid = false;
+      errors.password = "* Password is required";
+    }
+
+    setErrors(errors);
+    return isFormValid;
+  };
 
   const handleSubmit = () => {
-    storeData('userCreds', userInfo);
-  }
-  
+    if (validateForm()) {
+      storeData("userCreds", userInfo);
+      navigate("/dashboard");
+    }
+  };
+
   return (
     <div>
-      <form>
-        <label>
-          User Name:
-        </label>
-        <input type="text" id="username" name="username" onChange={(e) => handleChange(e)} />
-        <br></br>
-        <label>
-          Email:
-        </label>
-        <input type="text" name="email" onChange={handleChange} />
-        <br></br>
-        <label>
-          Password:
-        </label>
-        <input type="password" name="password" onChange={handleChange} />
-        <Button ButtonText="Login" onClick={handleSubmit} />
-      </form>
+      {/* <form> */}
+      <label>User Name:</label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        onChange={(e) => handleChange(e)}
+      />
+
+      <br></br>
+      <label>Email:</label>
+      <input type="text" name="email" onChange={handleChange} />
+      <br></br>
+      {errors.email && (
+        <span style={{ color: "red", fontSize: "10px" }}>{errors.email}</span>
+      )}
+
+      <br></br>
+      <label>Password:</label>
+      <input type="password" name="password" onChange={handleChange} />
+      <br></br>
+      {errors.password && (
+        <span style={{ color: "red", fontSize: "10px" }}>
+          {errors.password}
+        </span>
+      )}
+      <br></br>
+
+      <input type="checkbox" name="rememberMe" onChange={handleChange} />
+      <label htmlFor="checkbox">Remember me</label>
+
+      <Button ButtonText="Login" onClick={handleSubmit} />
+      {/* </form> */}
     </div>
   );
 }
